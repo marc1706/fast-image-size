a6827c816b84fbd800e7f5ea206c2580e363211a
Marc Alexander <admin@m-a-styles.de>
2015-04-07 11:13:47 +0200

    Add files for travis

---
+++ travis/run-phpunit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This file is part of the phpBB Forum Software package.
+#
+# @copyright (c) phpBB Limited <https://www.phpbb.com>
+# @license GNU General Public License, version 2 (GPL-2.0)
+#
+# For full copyright and license information, please see
+# the docs/CREDITS.txt file.
+#
+set -e
+set -x
+
+TRAVIS_PHP_VERSION=$1
+
+if [ "$TRAVIS_PHP_VERSION" == "5.6" ]
+then
+	phpunit --coverage-clover=coverage.clover
+	wget https://scrutinizer-ci.com/ocular.phar
+	php ocular.phar code-coverage:upload --format=php-clover coverage.clover
+else
+	phpunit
+fi

975888f66780743795397383a527a2557394963b
Marc Alexander <admin@m-a-styles.de>
2015-04-07 10:56:31 +0200

    Use valid namespace and add base file

---
+++ lib/fastImageSize.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * fast-image-size base class
+ * @package fast-image-size
+ * @copyright (c) Marc Alexander <admin@m-a-styles.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace fastImageSize;
+
+class fastImageSize
+{
+
+}

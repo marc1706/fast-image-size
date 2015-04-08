9ba0a67511796083dc30e96dfaacfecdad53324a
Marc Alexander <admin@m-a-styles.de>
2015-04-08 11:48:19 +0200

    Make sure supplied image returns valid dimensions for WBMP

--- lib/type/typeWbmp.php
+++ lib/type/typeWbmp.php
@@ -21,14 +21,45 @@ class typeWbmp extends typeBase
 		$data = $this->fastImageSize->get_image($filename, 0, self::LONG_SIZE);
 
 		// Check if image is WBMP
-		if (ord($data[0]) !== 0 || ord($data[1]) !== 0 || $data === substr(typeJp2::JPEG_2000_SIGNATURE, 0, 4))
+		if (!$this->validWBMP($data))
 		{
 			return;
 		}
 
 		$size = unpack('Cwidth/Cheight', substr($data, self::SHORT_SIZE, self::SHORT_SIZE));
 
+		// Check if dimensions are valid. A file might be recognised as WBMP
+		// rather easily (see extra check for JPEG2000).
+		if (!$this->validDimensions($size))
+		{
+			return;
+		}
+
 		$this->fastImageSize->set_size($size);
 		$this->fastImageSize->set_image_type(IMAGETYPE_WBMP);
+	}
+
+	/**
+	 * Return if supplied data might be part of a valid WBMP file
+	 *
+	 * @param string $data
+	 *
+	 * @return bool True if data might be part of a valid WBMP file, else false
+	 */
+	protected function validWBMP($data)
+	{
+		return ord($data[0]) === 0 && ord($data[1]) === 0 && $data !== substr(typeJp2::JPEG_2000_SIGNATURE, 0, self::LONG_SIZE);
+	}
+
+	/**
+	 * Return whether dimensions are valid
+	 *
+	 * @param array $size Size array
+	 *
+	 * @return bool True if dimensions are valid, false if not
+	 */
+	protected function validDimensions($size)
+	{
+		return $size['height'] > 0 && $size['width'] > 0;
 	}
 }
